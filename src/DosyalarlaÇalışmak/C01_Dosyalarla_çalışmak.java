7679580e67a66ae14159673015919912284f83db
ismail5240 <arkd50279@gmail.com>
2022-11-27 00:26:00 +0300

    Initial commit

---
+++ src/DosyalarlaÇalışmak/C01_Dosyalarla_çalışmak.java
@@ -0,0 +1,26 @@
+package DosyalarlaÇalışmak;
+
+import java.io.File;
+import java.io.IOException;
+
+public class C01_Dosyalarla_çalışmak {
+    public static void main(String[] args) {
+
+        File file = new File("C:\\Users\\ASUS\\IdeaProjects\\BTK\\file\\students.txt");
+
+        try {
+
+            if (file.createNewFile()) {
+
+                System.out.println("Dosya oluşturuldu");
+            } else {
+                System.out.println("Dosya zaten mevcut");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+
+    }
+}
---
+++ src/DosyalarlaÇalışmak/C02_Dosyayı_Okumak.java
@@ -0,0 +1,45 @@
+package DosyalarlaÇalışmak;
+
+import java.io.File;
+import java.io.IOException;
+
+public class C02_Dosyayı_Okumak {
+    public static void main(String[] args) {
+
+        getfileinfo();
+
+
+    }// main sonu
+
+    public static void createfile() {
+
+        File file = new File("C:\\Users\\ASUS\\IdeaProjects\\BTK\\file\\students.txt");
+
+        try {
+
+            if (file.createNewFile()) {
+
+                System.out.println("Dosya oluşturuldu");
+            } else {
+                System.out.println("Dosya zaten mevcut");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    public static void getfileinfo() {
+        File file = new File("C:\\Users\\ASUS\\IdeaProjects\\BTK\\file\\students.txt");
+        if (file.exists()) {
+            System.out.println("Dosya adı:" + file.getName());
+            System.out.println("Dosya yolu:" + file.getAbsolutePath());
+            System.out.println("Dosya yazılabilirmi:" + file.canWrite());
+            System.out.println("Dosya okunabilirmi:" + file.canRead());
+            System.out.println("Dosya boyutu(byte):" + file.length());
+        } else {
+            System.out.println("");
+        }
+    }
+}
