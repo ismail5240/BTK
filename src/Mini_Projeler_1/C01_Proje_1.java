0d736ac45ac05f9b2c36bd14cef409e65953cec5
ismail5240 <arkd50279@gmail.com>
2022-11-28 01:45:06 +0300

    28/11/22

---
+++ src/Mini_Projeler_1/C01_Proje_1.java
@@ -0,0 +1,26 @@
+package Mini_Projeler_1;
+
+public class C01_Proje_1 {
+    public static void main(String[] args) {
+
+        int sayi = 1;
+
+        boolean asal = true;
+        if (sayi < 2) {
+            System.out.println("Geçersiz sayı");
+            return;
+        }
+
+        for (int i = 2; i < sayi; i++) {
+            if (sayi % i == 0) {
+                asal = false;
+            }
+
+        }
+        if (asal) {
+            System.out.println("Sayı Asaldır:" + sayi);
+        } else {
+            System.out.println("Sayı Asal degildir");
+        }
+    }
+}
---
+++ src/Mini_Projeler_1/C02_Proje_2.java
@@ -0,0 +1,8 @@
+package Mini_Projeler_1;
+
+public class C02_Proje_2 {
+    public static void main(String[] args) {
+
+
+    }
+}
